3333e9b94fca9ba221e362052c8947054ea8416d
Christopher Callender <christophercallender0000@gmail.com>
2022-05-31 15:06:07 -0400

    Completed assignment

--- src/Child.js
+++ src/Child.js
@@ -1,8 +1,20 @@
-import React from "react";
-import { getRandomColor } from "./randomColorGenerator.js";
+import React from 'react';
+import { getRandomColor } from './randomColorGenerator.js';
 
-function Child({ onChangeColor }) {
-  return <div className="child" style={{ backgroundColor: "#FFF" }} />;
+//parent passes props to child (handleChangeColor, childrenColor)
+function Child({ handleChangeColor, childrenColor }) {
+   function handleClick() {
+      //child passes params to parent (getRandomColor() as newChildColor)
+      handleChangeColor(getRandomColor());
+   }
+
+   return (
+      <div
+         onClick={handleClick}
+         className="child"
+         style={{ backgroundColor: childrenColor }}
+      />
+   );
 }
 
 export default Child;
--- src/Parent.js
+++ src/Parent.js
@@ -1,17 +1,31 @@
-import React, { useState } from "react";
-import { getRandomColor } from "./randomColorGenerator.js";
-import Child from "./Child";
+import React, { useState } from 'react';
+import { getRandomColor } from './randomColorGenerator.js';
+import Child from './Child';
 
 function Parent() {
-  const randomColor = getRandomColor();
-  const [color, setColor] = useState(randomColor);
+   const randomColor = getRandomColor();
+   const [color, setColor] = useState(randomColor);
+   const [childrenColor, setChildrenColor] = useState('#FFF');
 
-  return (
-    <div className="parent" style={{ backgroundColor: color }}>
-      <Child />
-      <Child />
-    </div>
-  );
+   //child passes params to parent (getRandomColor() as newChildColor)
+   function handleChangeColor(newChildColor) {
+      setColor(getRandomColor());
+      setChildrenColor(newChildColor);
+   }
+
+   return (
+      //parent passes props to child (handleChangeColor, childrenColor)
+      <div className="parent" style={{ backgroundColor: color }}>
+         <Child
+            handleChangeColor={handleChangeColor}
+            childrenColor={childrenColor}
+         />
+         <Child
+            handleChangeColor={handleChangeColor}
+            childrenColor={childrenColor}
+         />
+      </div>
+   );
 }
 
 export default Parent;
